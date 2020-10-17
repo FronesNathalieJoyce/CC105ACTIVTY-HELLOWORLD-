7c2633c7f0615c8099f6d8db32894bf0d52a50b0
FronesNathalieJoyce <sm201600099@wmsu.edu.ph>
2020-10-17 12:26:43 +0800

    I am Rich with Image.

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,18 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text('Hello World'),
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: Text('I am Rich'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image(
+            image: NetworkImage(
+                'http://www.clker.com/cliparts/g/e/g/Y/L/9/ruby-one-hi.png'),
+          ),
+        ),
       ),
     ),
   );
