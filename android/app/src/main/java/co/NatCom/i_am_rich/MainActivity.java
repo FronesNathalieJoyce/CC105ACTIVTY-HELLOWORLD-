b2726222e86e5f6cfc307babeb01af9d8c8ce977
FronesNathalieJoyce <sm201600099@wmsu.edu.ph>
2020-10-17 10:37:49 +0800

    Hello World

---
+++ android/app/src/main/java/co/NatCom/i_am_rich/MainActivity.java
@@ -0,0 +1,6 @@
+package co.NatCom.i_am_rich;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+//the main function is the starting for all our apps.
+void main() {
+  runApp(
+    MaterialApp(
+      home: Center(
+        child: Text('Hello World'),
+      ),
+    ),
+  );
+}
